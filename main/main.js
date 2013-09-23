e288bbf821967b37f29c1be85b7b5caad8192bca
okimoto-koki <b211017@cc.it-hiroshima.ac.jp>
2013-09-24 02:27:20 +0900

    とりあえずプレイヤー機表示

--- main/main.js
+++ main/main.js
@@ -2,5 +2,17 @@ enchant();
 
 window.onload = function(){
 	game = new Game(320 , 320);
-	game.fps = 24;
-}
+	game.fps = 30;
+	game.preload('graphic.png');
+
+	game.onload = function() {
+		player = new Sprite(16, 16);
+		player.image = game.assets["graphic.png"];
+		player.x = 0;
+		player.y = 0;
+
+		game.rootScene.addChild(player);
+	}
+
+	game.start();
+};
