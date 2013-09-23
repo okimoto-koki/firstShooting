7195b62a472f9595804582c646c40a824e9f191e
okimoto-koki <b211017@cc.it-hiroshima.ac.jp>
2013-09-24 02:41:25 +0900

    移動をカーソルキーで出来るようにした

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ enchant();
 
 window.onload = function(){
 	game = new Game(320 , 320);
-	game.fps = 30;
+	game.fps = 60;
 	game.preload('graphic.png');
 
 	game.onload = function() {
@@ -12,6 +12,13 @@ window.onload = function(){
 		player.y = 0;
 
 		game.rootScene.addChild(player);
+
+		player.on('enterframe', function(){
+			if (game.input.right) this.x += 3;
+			if (game.input.left) this.x -= 3;
+			if (game.input.down) this.y += 3;
+			if (game.input.up) this.y -= 3;
+		});
 	}
 
 	game.start();
